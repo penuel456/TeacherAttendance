e2377e5ad0bbafc3fb6ea6a35b25241b12ddf0b1
LUCI <penuelcalle@gmail.com>
2019-02-07 10:48:42 +0800

    Login UI and variables

--- app/src/main/java/usc/dcis/teacherattendancesystem/MainActivity.kt
+++ app/src/main/java/usc/dcis/teacherattendancesystem/MainActivity.kt
@@ -2,11 +2,28 @@ package usc.dcis.teacherattendancesystem
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+
+    }
+
+    fun check(view: View){
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+
+        // check username and password first
+    }
+
+    fun login(){
+
     }
 }
