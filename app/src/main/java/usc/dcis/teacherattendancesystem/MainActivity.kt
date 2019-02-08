40c155a1ae4b03f3734cf856e021a8178e34596b
dreaminaway <hazydreams620@gmail.com>
2019-02-08 15:37:30 +0800

    Fixed the crashing bug and added an isBlank condition to the login page.

--- app/src/main/java/usc/dcis/teacherattendancesystem/MainActivity.kt
+++ app/src/main/java/usc/dcis/teacherattendancesystem/MainActivity.kt
@@ -19,26 +19,29 @@ class MainActivity : AppCompatActivity() {
 
         //val loginBtn = findViewById<Button>(R.id.loginBtn)
 
-        loginBtn.setOnClickListener {
-            var status=if(username.text.toString().toInt().equals(15102593)
-                && password.text.toString().equals("test123")) "Logged in Successfully" else "Incorrect username/password."
 
-          val text =  Toast.makeText(this, status, Toast.LENGTH_SHORT)
-            text.show()
-            check()
-        }
     }
 
-    fun check(){
-        val username = findViewById<EditText>(R.id.username)
-        val password = findViewById<EditText>(R.id.password)
+    fun check(view : View){
+        //val username = findViewById<EditText>(R.id.username)
+        //val password = findViewById<EditText>(R.id.password)
 
         // check username and password first
 
-        // Create an Intent to start the second activity
-        val menuActivity = Intent(this, Menu::class.java)
+        val status = if (!username.text.toString().isBlank() && username.text.toString().toInt().equals(15102593)
+            && password.text.toString().equals("test123")) "Logged in Successfully"
+        else if (username.text.toString().isBlank() || password.text.toString().isBlank()) "Please enter an input in the empty fields."
+        else "Incorrect username/password."
 
-        // Start the new activity.
-        startActivity(menuActivity)
+        val text = Toast.makeText(this, status, Toast.LENGTH_SHORT)
+        text.show()
+
+        // Create an Intent to start the second activity
+        if (!username.text.toString().isBlank() && username.text.toString().toInt().equals(15102593) && password.text.toString().equals("test123")) {
+            val menuActivity = Intent(this, Menu::class.java)
+
+            // Start the new activity.
+            startActivity(menuActivity)
+        }
     }
 }
