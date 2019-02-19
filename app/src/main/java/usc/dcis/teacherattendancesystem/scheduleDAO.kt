a4a62bcac87452b1454a4c68b8973e7f608a499b
LUCI <penuelcalle@gmail.com>
2019-02-19 17:12:44 +0800

    Working schedule database (not everything is tested yey)

--- app/src/main/java/usc/dcis/teacherattendancesystem/scheduleDAO.kt
+++ app/src/main/java/usc/dcis/teacherattendancesystem/scheduleDAO.kt
@@ -1,26 +1,44 @@
 package usc.dcis.teacherattendancesystem
 
 import android.arch.persistence.room.*
+import java.sql.Date
 
 
 @Dao
 interface ScheduleDAO {
+    // ALL SCHEDULEDB QUERIES
     @Insert
     fun insert(vararg schedules: scheduleDB)
 
     @Update
     fun update(vararg schedules: scheduleDB)
 
-
-    @Query("SELECT * FROM schedule")
+    @Query("SELECT * FROM Schedules")
     fun getAllSchedules(): List<scheduleDB>
 
-    @Query("SELECT * FROM schedule WHERE days = :selectedDays AND courseCode = :courseCode")
-    fun getSelectedDays(selectedDays: String, courseCode: String): scheduleDB
+    @Query("SELECT * FROM Schedules WHERE courseID = :courseId")
+    fun getSchedule(courseId: Int): scheduleDB
 
-    @Query("SELECT * FROM schedule WHERE days = :startTime AND courseCode = :courseCode")
-    fun getSelectedStartTime(startTime: String, courseCode: String): scheduleDB
+    // ALL ROOM ASSIGNMENT QUERIES
+    @Insert
+    fun insertRoomAssignment(vararg roomAssignment: RoomAssignment)
 
-    @Query("SELECT * FROM schedule WHERE days = :endTime AND courseCode = :courseCode")
-    fun getSelectedEndTime(endTime: String, courseCode: String): scheduleDB
+    @Update
+    fun updateRoomAssignment(vararg roomAssignment: RoomAssignment)
+
+    // ALL STATUS QUERIES
+    @Insert
+    fun insertStatus(vararg status: Status)
+
+    @Update
+    fun updateStatus(vararg status: Status)
+
+    @Query("SELECT * FROM Statuses WHERE roomID = :statusId")
+    fun getAllStatusByStatusId(statusId: Int): List<Status>
+
+    @Query("SELECT * FROM Statuses WHERE roomID = :roomId")
+    fun getAllStatusByRoomId(roomId: Int): List<Status>
+
+    @Query("SELECT * FROM Statuses WHERE status = :status")
+    fun getAllStatusByStatus(status: String): List<Status>
 }
--- app/src/main/java/usc/dcis/teacherattendancesystem/scheduleDB.kt
+++ app/src/main/java/usc/dcis/teacherattendancesystem/scheduleDB.kt
@@ -1,92 +1,51 @@
 package usc.dcis.teacherattendancesystem
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.ForeignKey.CASCADE
 import android.arch.persistence.room.PrimaryKey
 import android.support.annotation.NonNull
+import java.sql.Date
 
-@Entity(tableName = "Schedule")
-class scheduleDB {
-    var teacher: String = ""
-    var room: ArrayList<String>? = null
-    var startTime: ArrayList<String>? = null
-    var endTime: ArrayList<String>? = null
-    var days: ArrayList<String>? = null
-    var status: ArrayList<String>? = null
+@Entity(tableName = "Schedules")
+data class scheduleDB(
+    @PrimaryKey(autoGenerate = true) val courseID: Int,
+    @ColumnInfo(name = "course_code")var courseCode: String,
+    @ColumnInfo(name = "teacher") var teacher: String
+)
 
-    @PrimaryKey
-    @NonNull
-    var courseCode: String = ""
 
-    @NonNull
-    fun getCoursecode(): String {
-        return courseCode
-    }
+// ROOM ASSIGNMENTS
+@Entity(tableName = "Room_Assignments", foreignKeys = arrayOf(ForeignKey(entity = scheduleDB::class,
+    parentColumns = arrayOf("courseID"),
+    childColumns = arrayOf("courseID"),
+    onDelete = ForeignKey.CASCADE,
+    onUpdate = ForeignKey.CASCADE)))
+data class RoomAssignment(
+    @PrimaryKey(autoGenerate = true) val roomID: Int,
+    // FOREIGN KEY
+    val courseID: Int,
 
-    fun setCoursecode(courseCode : String) {
-        this.courseCode = courseCode
-    }
+    var roomNumber: String,
+    var startTime: Date,
+    var endTime: Date,
 
-    fun getroom(): ArrayList<String>? {
-        return room
-    }
+    // MWF, TTH, etc
+    var dayAssigned: String
+)
 
-    fun setroom(room: ArrayList<String>?){
-        this.room = room
-    }
+@Entity(tableName = "Statuses", foreignKeys = arrayOf(ForeignKey(entity = RoomAssignment::class,
+    parentColumns = arrayOf("roomID"),
+    childColumns = arrayOf("roomID"),
+    onDelete = ForeignKey.CASCADE,
+    onUpdate = ForeignKey.CASCADE)))
+data class Status(
+    // FOREIGN KEY
+    @PrimaryKey(autoGenerate = true) val statusId: Int,
+    val roomID: Int,
 
-    fun getSingleRoom(selectedRoom: Int): String? {
-        return room?.get(selectedRoom)
-    }
-
-    fun setSingleRoom(selectedRoom: Int, newRoom: String){
-        this.room?.set(selectedRoom, newRoom)
-    }
-
-    fun getStarttime(): MutableList<String>? {
-        return startTime
-    }
-
-    fun setStarttime(startTime: ArrayList<String>?) {
-        this.startTime = startTime
-    }
-
-    fun getEndtime(): ArrayList<String>? {
-        return endTime
-    }
-
-    fun setEndtime(endTime: ArrayList<String>?){
-        this.endTime = endTime
-    }
-
-    fun getdays(): ArrayList<String>? {
-        return days
-    }
-
-    fun setdays(days: ArrayList<String>?){
-        this.days = days
-    }
-
-    fun getSingleDay(selectedDay: Int): String {
-        return this!!.days!![selectedDay]
-    }
-
-    fun setSingleDay(selectedDay: Int, newDay: String){
-        this.days?.set(selectedDay, newDay)
-    }
-
-    fun getstatus(): ArrayList<String>? {
-        return status
-    }
-
-    fun setstatus(days: ArrayList<String>?){
-        this.status = status
-    }
-
-    fun getSingleStatus(selectedStatus: Int): String {
-        return this!!.status!![selectedStatus]
-    }
-
-    fun setSingleStatus(selectedStatus: Int, newStatus: String){
-        this.status?.set(selectedStatus, newStatus)
-    }
-}
+    // FOR KNOWING WHICH DAY IS PRESENT OR ABSENT
+    var date: Date,
+    var status: String
+)
