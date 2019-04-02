3d54369c22689e75a996a4789a5a2210aa63b05e
dreaminaway <hazydreams620@gmail.com>
2019-04-02 18:02:32 +0800

    RoomSchedule activity lists down only today's schedule of that specific room instead of it's entire schedule.

--- app/src/main/java/usc/dcis/teacherattendancesystem/menu/Menu_fragment_lbb.kt
+++ app/src/main/java/usc/dcis/teacherattendancesystem/menu/Menu_fragment_lbb.kt
@@ -534,6 +534,7 @@ class Menu_fragment_lbb : Fragment() {
         )
 
         */
+
         //endregion
         //ScheduleFirebase.AddMultipleRoomAssignments(FirebaseFirestore.getInstance(), scheduleListTest.getAllRoomAssignments())
 
